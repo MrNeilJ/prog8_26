7ef9ead664e0ea79aa2bd45cd432628ab9d2ca17
neiljohnson <mr.neil.j@gmail.com>
2017-02-15 16:41:41 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// This program stores employee hours worked and hourly pay rates
+// in two vectors.  It demonstrates the use of the push_back member
+// function to add new elements to the vectors
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+}
